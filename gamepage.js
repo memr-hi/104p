b4d92e4e59c294634a931ffbe93fb6fe380d6c55
Memr! <126511493+memr-hi@users.noreply.github.com>
2023-08-07 15:49:32 -0300

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,32 @@
+
+var y = Math.floor(Math.random() * 10 + 1);
+
+playername= localStorage.getItem("player_name")    
+
+var guess = 1;
+  
+function submit(){
+  
+// number guessed by user     
+var x = document.getElementById("guessField").value;
+
+if(x == y)
+{    
+   alert("PARABÉNS!!!"+playername+", você acertou em "
+           + guess + " tentativa(s)! ");
+    guess= 1;
+}
+else if(x > y)  
+{    
+   guess++;
+   alert("Opa, resposta errada!! Tente um número menor");
+}
+else
+{
+   guess++;
+   alert("Opa, resposta errada!! Tente um número maior")
+}
+}
+function playAgain(){
+    y = Math.floor(Math.random() * 10 + 1);
+}
